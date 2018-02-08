32c4f4e4adabfa7932d583bb2c4553d1a10b7d55
Francisco Zuccalá <francisco.zuccala@gmail.com>
2018-02-08 14:39:50 -0300

    Modified test

---
+++ src/test/groovy/com/github/franciscozuccala/inhibitor/InhibitorPluginTest.groovy
@@ -0,0 +1,27 @@
+package com.github.franciscozuccala.inhibitor
+
+import com.github.franciscozuccala.inhibitor.tasks.ConfigureLocalMavenRepositoryTask
+import com.github.franciscozuccala.inhibitor.tasks.ConfigureNexusTask
+import com.github.franciscozuccala.inhibitor.tasks.SaveNexusTask
+import com.github.franciscozuccala.inhibitor.tasks.StartNexusTask
+import com.github.franciscozuccala.inhibitor.tasks.StopNexusTask
+import org.gradle.api.Project
+import org.gradle.testfixtures.ProjectBuilder
+import org.junit.*
+
+import static org.gradle.internal.impldep.org.junit.Assert.assertTrue
+
+class InhibitorPluginTest {
+
+    @Test void inhibitorAarPluginAddsUploadDependenciesByGroupTaskToProject(){
+        Project project = ProjectBuilder.builder().build()
+        project.pluginManager.apply 'inhibitor'
+
+        assertTrue(project.tasks.configureLocalMavenNexus instanceof ConfigureLocalMavenRepositoryTask)
+        assertTrue(project.tasks.configureNexus instanceof ConfigureNexusTask)
+        assertTrue(project.tasks.saveNexus instanceof SaveNexusTask)
+        assertTrue(project.tasks.startNexus instanceof StartNexusTask)
+        assertTrue(project.tasks.stopNexus instanceof StopNexusTask)
+
+    }
+}
