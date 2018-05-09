5d28deac14641eeb64e2f16e28d09d8ba9a54344
Francisco Zuccalá <francisco.zuccala@gmail.com>
2018-05-09 09:25:25 -0300

    Renamed method

--- src/main/groovy/com/github/franciscozuccala/inhibitor/InhibitorPlugin.groovy
+++ src/main/groovy/com/github/franciscozuccala/inhibitor/InhibitorPlugin.groovy
@@ -19,16 +19,16 @@ class InhibitorPlugin implements Plugin<Project> {
         project.task('stopNexus', type: StopNexusTask).dependsOn('configureNexus')
 
         project.afterEvaluate {
-            if (getProperty(project, 'ENABLE_START_NEXUS', false)) {
+            if (getBooleanEnvironmentProperty(project, 'ENABLE_START_NEXUS', false)) {
                 project.tasks.configureNexus.execute()
                 project.tasks.startNexus.execute()
-            }else if (getProperty(project, 'CONFIGURE_LOCAL_MAVEN_REPOSITORY', false)){
+            }else if (getBooleanEnvironmentProperty(project, 'CONFIGURE_LOCAL_MAVEN_REPOSITORY', false)){
                 project.tasks.configureLocalMavenRepository.execute()
             }
         }
     }
 
-    private static Boolean getProperty(Project project, String propertyName, Boolean defaultValue){
+    private static Boolean getBooleanEnvironmentProperty(Project project, String propertyName, Boolean defaultValue){
         try {
             if (project.hasProperty(propertyName)){
                 return project.ext.get(propertyName)
