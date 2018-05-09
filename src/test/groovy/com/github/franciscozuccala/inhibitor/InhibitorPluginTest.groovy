3eed19d42f15b1962cfc75267d86ccb5ee2cc09d
Francisco Zuccalá <francisco.zuccala@gmail.com>
2018-05-09 11:06:07 -0300

    Added test

--- src/test/groovy/com/github/franciscozuccala/inhibitor/InhibitorPluginTest.groovy
+++ src/test/groovy/com/github/franciscozuccala/inhibitor/InhibitorPluginTest.groovy
@@ -5,7 +5,9 @@ import com.github.franciscozuccala.inhibitor.tasks.ConfigureNexusTask
 import com.github.franciscozuccala.inhibitor.tasks.SaveNexusTask
 import com.github.franciscozuccala.inhibitor.tasks.StartNexusTask
 import com.github.franciscozuccala.inhibitor.tasks.StopNexusTask
+import org.gradle.api.InvalidUserDataException
 import org.gradle.api.Project
+import org.gradle.api.tasks.TaskValidationException
 import org.gradle.testfixtures.ProjectBuilder
 import org.junit.*
 
@@ -13,7 +15,7 @@ import static org.gradle.internal.impldep.org.junit.Assert.assertTrue
 
 class InhibitorPluginTest {
 
-    @Test void inhibitorAarPluginAddsUploadDependenciesByGroupTaskToProject(){
+    @Test void inhibitorPluginAddsAllTasksToProject(){
         Project project = ProjectBuilder.builder().build()
         project.pluginManager.apply 'inhibitor'
 
@@ -23,5 +25,22 @@ class InhibitorPluginTest {
         assertTrue(project.tasks.startNexus instanceof StartNexusTask)
         assertTrue(project.tasks.stopNexus instanceof StopNexusTask)
 
+    }
+
+    @Test(expected = TaskValidationException.class) void inhibitorPluginRunsConfigureLocalMavenRepositoryThrowsNexusRepositoryNotDefined(){
+        Project project = ProjectBuilder.builder().build()
+        project.pluginManager.apply 'inhibitor'
+
+        project.tasks.configureLocalMavenRepository.execute()
+
+    }
+
+    @Test(expected = TaskValidationException.class) void inhibitorPluginRunsConfigureLocalMavenRepositoryConfiguredOk(){
+        Project project = ProjectBuilder.builder().build()
+        project.pluginManager.apply 'inhibitor'
+
+        project.tasks.configureLocalMavenRepository{
+            it.nexusRepository=""
+        }
     }
 }
